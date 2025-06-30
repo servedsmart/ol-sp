cf6bc142486b28ce583b085ff95afb4561115345
Served Smart <195884188+servedsmart@users.noreply.github.com>
2025-06-30 06:05:21 +0200

    :memo: Improve documentation

--- src/main.js
+++ src/main.js
@@ -20,12 +20,16 @@ import View from "ol/View.js";
 
 /**
  * @classdesc
- * A Control with a button to set a new center of the view.
+ * A `Control` with a button to set a new center of the view.
  */
 class CenterControl extends Control {
   /**
    *
-   * @param {{ target: string | HTMLElement | undefined, center: Coordinate | undefined, element: HTMLElement | undefined, button: HTMLElement | undefined }} options Control options. Takes target, center, element and button.
+   * @param {Object} config Configuration `Object`.
+   * @param {string | HTMLElement | undefined} config.target The target of the `Control`.
+   * @param {import("ol/coordinate").Coordinate | undefined} config.center The center that should be set for the `View` of the `Map`.
+   * @param {HTMLElement | undefined} config.element The element that is used as `Control`.
+   * @param {HTMLElement | undefined} config.button The button element that is used as `Control`.
    */
   constructor({ target, center, element, button } = {}) {
     super({
@@ -68,10 +72,10 @@ function loadStylesheet(stylesheet, stylesheetHash) {
 }
 
 /**
- * Get a TileLayer with an OSM source.
- * @param {import("ol/source/Source").AttributionLike | undefined} attributions Attributions for the OSM source. Takes HTML code.
- * @param {string | undefined} url URL to source the OSM from.
- * @returns A TileLayer with an OSM source.
+ * Get a `TileLayer` with an `OSM` source.
+ * @param {import("ol/source/Source").AttributionLike | undefined} attributions Attributions for the `OSM` source. Takes HTML code.
+ * @param {string | undefined} url URL to source the `OSM` from.
+ * @returns {TileLayer<OSM>} A `TileLayer` with an `OSM` source from specified parameters.
  */
 function getTilelayer(attributions, url) {
   return new TileLayer({
@@ -84,16 +88,16 @@ function getTilelayer(attributions, url) {
 }
 
 /**
- * Get Map and apply styling.
- * @param {string | HTMLElement | undefined} target The target of the map.
- * @param {string | undefined} height The height of the map element.
- * @param {string | undefined} width The width of the map element.
- * @param {import("ol/coordinate").Coordinate | undefined} center The center of the map.
- * @param {number | undefined} maxZoom The maximum zoom of the map.
- * @param {number | undefined} minZoom The minimum zoom of the map.
- * @param {number | undefined} zoom The default zoom of the map.
- * @param {BaseLayer[] | Collection<BaseLayer> | LayerGroup | undefined} layers The layers of the map.
- * @returns A Map.
+ * Get `Map` and apply styling.
+ * @param {string | HTMLElement | undefined} target The target of the `Map`.
+ * @param {string | undefined} height The height of the `Map` element.
+ * @param {string | undefined} width The width of the `Map` element.
+ * @param {import("ol/coordinate").Coordinate | undefined} center The center of the `View`.
+ * @param {number | undefined} maxZoom The maximum zoom of the `View`.
+ * @param {number | undefined} minZoom The minimum zoom of the `View`.
+ * @param {number | undefined} zoom The default zoom of the `View`.
+ * @param {BaseLayer[] | Collection<BaseLayer> | LayerGroup | undefined} layers The layers of the `Map`.
+ * @returns {Map} A `Map` with a `View` from specified parameters.
  */
 function getStyledMap(target, height, width, center, maxZoom, minZoom, zoom, layers) {
   target.style.height = height ? height : width;
@@ -114,11 +118,11 @@ function getStyledMap(target, height, width, center, maxZoom, minZoom, zoom, lay
 }
 
 /**
- * Get CenterControl and apply styling.
- * @param {HTMLElement} button Button element to use in CenterControl.
- * @param {HTMLElement} element Element to use in CenterControl.
- * @param {import("ol/coordinate").Coordinate} center Center to use in CenterControl.
- * @returns A CenterControl.
+ * Get `CenterControl` and apply styling.
+ * @param {HTMLElement} button Button element to use in `CenterControl`.
+ * @param {HTMLElement} element Element to use in `CenterControl`.
+ * @param {import("ol/coordinate").Coordinate} center Center for a `View` of a `Map` to use in `CenterControl`.
+ * @returns {CenterControl} A `CenterControl` with specified parameters.
  */
 function getStyledCenterControl(button, element, center) {
   button.style.display = "block";
@@ -132,14 +136,14 @@ function getStyledCenterControl(button, element, center) {
 }
 
 /**
- * Get Overlay with specific properties that are suitable for point icons and apply styling.
+ * Get `Overlay` with specific properties that are suitable for point icons and apply styling.
  * @param {string} iconSize Size of the icon.
  * @param {HTMLElement | undefined} element Element containing the icon.
- * @param {import("ol/coordinate").Coordinate | undefined} position Position of the new Overlay.
- * @returns An Overlay with specific properties that are suitable for point icons.
+ * @param {import("ol/coordinate").Coordinate | undefined} position Position of the new `Overlay`.
+ * @returns {Overlay} An `Overlay` with specified parameters.
  */
 function getStyledIconOverlay(iconSize, element, position) {
-  const length = iconSize ? iconSize : "64px";
+  const length = iconSize;
   element.style.height = length;
   element.style.width = length;
   element.style.display = "block";
@@ -153,10 +157,10 @@ function getStyledIconOverlay(iconSize, element, position) {
 }
 
 /**
- * Get Overlay with specific properties that are suitable for popups and apply styling.
+ * Get `Overlay` with specific properties that are suitable for popups and apply styling.
  * @param {HTMLElement | undefined} element Element containing text for the popup.
- * @param {number[] | undefined} offset Offset to move the Overlay.
- * @returns An Overlay with specific properties that are suitable for popups.
+ * @param {number[] | undefined} offset Offset to move the `Overlay`.
+ * @returns {Overlay} An Overlay with specified parameters.
  */
 function getStyledPopupOverlay(element, offset) {
   element.style.display = "block";
@@ -172,8 +176,31 @@ function getStyledPopupOverlay(element, offset) {
   return overlay;
 }
 
+/**
+ *
+ * @param {Object} config Configuration `Object`.
+ * @param {string} config.mapId The id of the map element.
+ * @param {string | undefined} config.centerControlId The id of the `CenterControl` element.
+ * @param {string | undefined} config.centerControlButtonId The id of the `CenterControl` button element.
+ * @param {string | undefined} config.iconId The id of the icon element.
+ * @param {string | undefined} config.popupId The id of the popup element.
+ * @param {string | undefined} config.stylesheet The stylesheet to load.
+ * @param {string | undefined} config.stylesheetHash The hash of the stylesheet to load.
+ * @param {string | undefined} config.extraCopyrightURL The URL for an extra copyright attribution.
+ * @param {string | undefined} config.extraCopyrightName The name of the extra copyright attribution.
+ * @param {string} config.tileBaseURL The URL for the OSM tile server.
+ * @param {string} config.height The height of the map element.
+ * @param {string} config.width The width of the map element.
+ * @param {number} config.centerX The X coordinate (longitude) for the center of the map.
+ * @param {number} config.centerY The Y coordinate (latitude) for the center of the map.
+ * @param {number} config.zoom The default zoom level. Valid ranges are `0-20`. See https://wiki.openstreetmap.org/wiki/Zoom_levels.
+ * @param {number} config.minZoom The minimum zoom level. Valid ranges are `0-20`. See https://wiki.openstreetmap.org/wiki/Zoom_levels.
+ * @param {number} config.maxZoom The maximum zoom level. Valid ranges are `0-20`. See https://wiki.openstreetmap.org/wiki/Zoom_levels.
+ * @param {number | undefined} config.pointX The X coordinate (longitude) for the position of the point.
+ * @param {number | undefined} config.pointY The Y coordinate (latitude) for the position of the point.
+ * @param {string | undefined} config.iconSize The size of the icon element.
+ */
 window.olSp = (config) => {
-  // FIXME: Make most of these optional
   const {
     mapId,
     centerControlId,
